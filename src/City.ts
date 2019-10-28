e4e8496c382482dbfe6fb87f956d5748e50d4e90
Casper Stroemgren <casper@stroemgren.dk>
2019-10-28 21:56:15 +0100

    ..

--- src/City.ts
+++ src/City.ts
@@ -1,5 +1,13 @@
+const uuidv4 = require('uuid/v4')
+
 export const City = (id: string, name: string) => ({
     id, name
 })
 
 export interface City extends ReturnType<typeof City> {}
+
+export const CityForm = (name: string, country_id) => ({
+	id: 'ci_' + uuidv4(), name, country_id
+})
+
+export interface CityForm extends ReturnType<typeof CityForm> {}
