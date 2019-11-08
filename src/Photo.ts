d4ef9e5758b62fd1845afc19c64a139f7fd68d8e
Casper Stroemgren <casper@stroemgren.dk>
2019-11-08 14:59:37 +0100

    ..

---
+++ src/Photo.ts
@@ -0,0 +1,13 @@
+export const Photo = (photo_id: string) => {
+	const baseUrl: 'https://res.cloudinary.com/duob2cdih/image/upload'
+
+	return {
+		thumb: `${baseUrl}/c_scale,w_100/${photo_id}`,
+		small: `${baseUrl}/c_scale,w_400/${photo_id}`,
+		medium: `${baseUrl}/c_scale,w_1000/${photo_id}`,
+		large: `${baseUrl}/c_scale,w_2500/${photo_id}`,
+		original: `${baseUrl}/${photo_id}`
+	}
+}
+
+export interface Photo extends ReturnType<typeof Photo> {}
